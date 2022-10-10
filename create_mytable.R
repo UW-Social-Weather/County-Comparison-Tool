75e4ad58997b9ade5a4a4d1ee4cefdace2ab8907
Harshavardhini Renganathan <86983914+harshavardhini910@users.noreply.github.com>
2022-10-10 11:10:52 -0700

    Update create_mytable.R

--- create_mytable.R
+++ create_mytable.R
@@ -1,3 +1,5 @@
+#This is used to create a table of primary variables, secondary variables, state and county
+
 create_mytable <- function(all_data, sel_data, map_level, prim_var) {
   if (map_level == 1) {
     curr_gid <- "GID_1"

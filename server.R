b8ece53df198a9d4edbadfb9fe3c9c6e406ef3da
Harshavardhini Renganathan <86983914+harshavardhini910@users.noreply.github.com>
2022-10-10 11:15:51 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,6 @@
-#This is the server portion of the RShiny code
+# Name:Harsha
+# Date:10/10/2022
+# Purpose: This is the server portion of the RShiny code
 
 server <- function(input, output) {
   callModule(mod_healthdown, "Comparison Tool")

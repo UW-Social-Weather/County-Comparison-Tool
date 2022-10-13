f5b8f085956780a012447d70d659fa5838013ff6
caitlindrover <caitlindrover@gmail.com>
2022-10-13 11:33:28 -0700

    added chalmydia data/removed duplicate str_replace code

--- data/clean_data.R
+++ data/clean_data.R
@@ -63,25 +63,19 @@ for (x in 202:length(files)) {
     str_replace("Premature death:Years of Potential Life Lost Rate", "Premature death:YPLL Rate") %>% 
     str_replace("Low birthweight:LBW Births", "Low birthweight:# Low Birthweight Births") %>% 
     str_replace("Low birthweight:Live Births", "Low birthweight:# Live births") %>% 
+    str_replace("Low birthweight:# Live Births", "Low birthweight:# Live births") %>%  
     str_replace("Sexually transmitted infections:Chlamydia Cases", "Sexually transmitted infections:# Chlamydia Cases") %>% 
+    str_replace("Sexually transmitted infections:Chlamydia Rate", "Sexually transmitted infections:Chlamydia Incidence") %>% 
     str_replace("Primary care physicians:PCP", "Primary care physicians:# PCP") %>% 
-    str_replace("Preventable hospital stays:Preventable Hosp. Rate", "Preventable hospital stays:ACSC Rate") %>% 
     str_replace("Primary care physicians:# Primary Care Physicians", "Primary care physicians:# PCP") %>% 
+    str_replace("Preventable hospital stays:Preventable Hosp. Rate", "Preventable hospital stays:ACSC Rate") %>% 
     str_replace("Preventable hospital stays:# Medicare Enrollees", "Preventable hospital stays:# Medicare enrollees") %>%
+    str_replace("Preventable hospital stays:Medicare enrollees", "Preventable hospital stays:# Medicare enrollees") %>% 
     str_replace("Violent crime:# Violent Crimes", "Violent crime:# Annual Violent Crimes")%>%
     str_replace("Violent crime:Annual Violent Crimes", "Violent crime:# Annual Violent Crimes") %>%
     str_replace("Air pollution - particulate matter:Average Daily PM2.5", "Air pollution - particulate matter:Average daily PM25") %>%
     str_replace("Drinking water violations:% Pop in Viol", "Drinking water violations:% pop in viol" ) %>%
-    str_replace("Driving alone to work:Workers", "Driving alone to work:# Workers")%>% 
-    str_replace("Primary care physicians:PCP", "Primary care physicians:# PCP") %>% 
-    str_replace("Preventable hospital stays:Medicare enrollees", "Preventable hospital stays:# Medicare enrollees") %>% 
-    str_replace("Low birthweight:LBW Births", "Low birthweight:# Low Birthweight Births") %>% 
-    str_replace("Low birthweight:Live Births", "Low birthweight:# Live births") %>% 
-    str_replace("Sexually transmitted infections:Chlamydia Cases", "Sexually transmitted infections:# Chlamydia Cases") %>% 
-    str_replace("Primary care physicians:PCP", "Primary care physicians:# PCP") %>% 
-    str_replace("Violent crime:Annual Violent Crimes", "Violent crime:# Annual Violent Crimes") %>% 
     str_replace("Injury deaths:Injury Deaths", "Injury deaths:# Injury Deaths") %>%
-    str_replace("Low birthweight:# Live Births", "Low birthweight:# Live births") %>%
     str_replace("Poor or fair health:% Fair or Poor Health", "Poor or fair health:% Fair/Poor") %>%
     str_replace("Poor physical health days:Average Number of Physically Unhealthy Days", "Poor physical health days:Physically Unhealthy Days") %>%
     str_replace("Poor mental health days:Average Number of Mentally Unhealthy Days", "Poor mental health days:Mentally Unhealthy Days") %>%
@@ -91,8 +85,8 @@ for (x in 202:length(files)) {
     str_replace("Long commute - driving alone:Workers who Drive Alone", "Long commute - driving alone:# Workers who Drive Alone") %>%
     str_replace("Severe housing problems:# Household with Severe Problems" , "Severe housing problems:# Households with Severe Problems" )
     
-    
-    
+
+
   #trim empty lines
   file <- file[!is.na(file[,1]),]
   
